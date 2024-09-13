109adc709593cc0f6269bd279c2c2260f134cddc
SKitttz <skittz.dev@gmail.com>
2024-09-13 14:24:50 -0300

    feat(clothing): create clothing entity

---
+++ src/domain/entities/Clothing.ts
@@ -0,0 +1,10 @@
+export interface Clothing{
+    id: string;
+    name: string;
+    description: string;
+    material: string;
+    price: number;
+    sizes: string[];
+    colors: string[];
+    images: string[];
+}
